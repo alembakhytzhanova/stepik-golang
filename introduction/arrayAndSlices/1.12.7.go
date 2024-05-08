46fcb22a6d6e7f7467eaf45a49c7a55c7e450646
mirvision <florangorr@gmail.com>
2024-05-08 18:05:10 +0500

    1.12 Массивы и срезы

---
+++ introduction/arrayAndSlices/1.12.7.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var size int
+	fmt.Scan(&size)
+	arr := make([]int, size)
+	for i := 0; i < size; i++ {
+		fmt.Scan(&arr[i])
+	}
+	fmt.Println(arr[3])
+}
